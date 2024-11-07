f13b293f07016fa5a65f04db4c07965dee453ef6
EdenAlmighty <edengilady1@gmail.com>
2024-11-07 18:17:11 +0200

    Init repo

---
+++ src/components/AppFooter.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function AppFooter() {
+	return (
+		<footer>
+			<span>Footer</span>
+		</footer>
+	)
+}
---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function AppHeader() {
+    return (
+        <header>
+            <div className="logo-container">
+                <img src="/logo.png" alt="logo" className="logo" />
+                <h1>Header</h1>
+            </div>
+            <h1>Header</h1>
+            <nav>Nav</nav>
+        </header>
+    )
+}

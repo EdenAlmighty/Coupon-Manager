93619b31320e4196bdde78baeaea8ad98c66c644
EdenAlmighty <edengilady1@gmail.com>
2024-11-14 22:11:50 +0200

    Set repo base for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../backend/public',
-    emptyOutDir: true,
-  },
+  base: '/Coupon-Manager/'
 })

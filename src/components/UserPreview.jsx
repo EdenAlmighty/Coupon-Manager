67bbb62f762fbf080f2d12ad9937ff01c28bad1b
EdenAlmighty <edengilady1@gmail.com>
2024-11-17 01:12:26 +0200

    Add dist

--- src/components/UserPreview.jsx
+++
@@ -1,9 +0,0 @@
-export function UserPreview({ user }) {
-    return (
-        <>
-            <td>{user.fullname}</td>
-            <td>{user.username}</td>
-            <td>{user.isAdmin ? "Admin" : "User"}</td>
-        </>
-    )
-}
--- src/hooks/useLoading.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { LoadingContext } from "../context/LoadingContext";
-
-export function useLoading() {
-    return useContext(LoadingContext);
-  }

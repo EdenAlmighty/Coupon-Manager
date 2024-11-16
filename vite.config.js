4f6d1e7714e3672ad244ad8165b820964f40856b
EdenAlmighty <edengilady1@gmail.com>
2024-11-17 01:02:13 +0200

    Remove dist

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  outDir: 'dist',
-  emptyOutDir: true,
   base: '/Coupon-Manager/', 
   plugins: [react()],
 })
